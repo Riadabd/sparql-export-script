97bcaea5be7812a7f2b84356b3fd63c31f3390d3
Riad Abdallah <riad.abdallah@hotmail.com>
2023-07-19 13:52:42 +0300

    Generate turtle and graph files in separate folders

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 
 # VAR. DEFAULT
 FAILED=0
-OUT_FOLDER="tmp/out"
+OUT_FOLDER="tmp/"
 DEFAULT_EXPORT_FILENAME="export"
 SPARQL_ENDPOINT='http://localhost:8890/sparql'
 
@@ -30,23 +30,34 @@ while :; do
 done
 
 mkdir -p "$OUT_FOLDER"
-rm -rf "$OUT_FOLDER"/"$DEFAULT_EXPORT_FILENAME".ttl
+rm -rf "$OUT_FOLDER"/*
 
-for path in queries/*.sparql;do
+for path in queries/*.sparql; do
     filename=$(basename "$path" .sparql)
+
+    # Create a turtle file and its corresponding graph with the current timestamp
+    current_date=$(date '+%Y%m%d%H%M%S')
+    mkdir -p "$OUT_FOLDER"/"$current_date-$filename"
+    export_ttl_filename="$current_date-$filename.ttl"
+    export_graph_filename="$current_date-$filename.graph"
+
     query=$(cat "$path")
     echo "[INFO] Generating export for $filename ..."
     if curl --fail -X POST "$SPARQL_ENDPOINT" \
       -H 'Accept: text/plain' \
-      --form-string "query=$query" >> "$OUT_FOLDER"/export.ttl; then
+      --form-string "query=$query" >> "$OUT_FOLDER"/"$current_date-$filename"/"$export_ttl_filename"; then
       echo "[INFO] Finished export for $filename!"
+      touch "$OUT_FOLDER/$current_date-$filename/$export_graph_filename"
+      echo "[INFO] Created graph file for $filename!"
     else
       echo "[ERROR] export for $filename Failed!"
       FAILED+=1
     fi;
     echo "================================================================================"
 done
-echo "[INFO] Export Done! You can find your export in $OUT_FOLDER/export.ttl"
+
+echo "[INFO] Export Done! You can find your export in $OUT_FOLDER/$current_date-$filename/$export_ttl_filename"
+
 if ((FAILED > 0)); then
   echo "[WARNING] $FAILED queries failed, export incomplete ..."
 fi;
